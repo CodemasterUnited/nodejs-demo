7888ce289f8ad6fc72ae372f513a43e4339965c9
Codemaster United <108175342+CodemasterUnited@users.noreply.github.com>
2023-04-09 18:58:09 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ module.exports = (req, res) => {
   function npmls() {
     require('child_process').exec('npm ls --json', function(err, stdout, stderr) {
       if (err) return alert(err)
-      res.status(200).send(JSON.parse(stdout));
+      res.status(200).send(stdout);
     });
   }
   npmls();

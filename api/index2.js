267fbf126cc459df6e8c576002ff975fdff92052
Codemaster United <108175342+CodemasterUnited@users.noreply.github.com>
2023-04-11 21:08:41 +0530

    Update index2.js

--- api/index2.js
+++ api/index2.js
@@ -1,5 +1,4 @@
 module.exports = (req, res) => {
-	var require('child_process').exec('npm ls --json',
 	res.status(200).send('Helllllo');
 	require('child_process').exec('npm ls --json', function(err, stdout, stderr) {
 		if (err) return res.status(200).send(err);

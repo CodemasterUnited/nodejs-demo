ecd4df9b10a4c87679cc66852cf7116cd573db69
Codemaster United <108175342+CodemasterUnited@users.noreply.github.com>
2023-04-11 21:10:10 +0530

    Update index2.js

--- api/index2.js
+++ api/index2.js
@@ -1,6 +1,6 @@
 module.exports = (req, res) => {
 	res.status(200).send('Helllllo');
-	require('child_process').exec('npm ls --json', function(err, stdout, stderr) {
+	require('child_process').exec('npm install camelcase', function(err, stdout, stderr) {
 		if (err) return res.status(200).send(err);
 		res.status(200).send(JSON.parse(stdout));
 	});
